ba1b03a855e9c1f0ba86e2efe52590521de2632a
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-01-21 17:10:23 -0500

    part 1 - step 5 completed

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const app = express()
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
-})
+});
+
+
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+});
+
 
 app.listen(process.env.PORT);
