a72560d2cdcf0bbb2238064515e8304cbb5ab917
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-01-21 16:48:45 -0500

    part 1- step 4 completed

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
+
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.listen(3000)
+app.listen(process.env.PORT);
