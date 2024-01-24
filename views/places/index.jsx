d7165db32e42179e9dd008a2b8be1d41c7259b5a
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-01-23 21:38:55 -0500

    part 3 - step 5 completed

---
+++ views/places/index.jsx
@@ -0,0 +1,16 @@
+const React = require('react');
+const Def = require('../default');
+
+
+function places () {
+    return (
+        <Def>
+          <main>
+              <h1>PLACES</h1>
+          </main>
+        </Def>
+    )
+};
+
+
+module.exports = places;
