ddf2e77b4e0ed752cee06788b80e9402b06fe573
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-01-25 18:20:54 -0500

    part 3 - step 6 completed

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,14 +2,24 @@ const React = require('react');
 const Def = require('../default');
 
 
-function places () {
+function places (data) {
+    let placesFormatted = data.places.map((place) => {
+        return (
+            <div>
+                <h2>{place.name}</h2>
+                <img src={place.pic} alt={place.name}></img>
+            </div>
+        )
+    });
+
     return (
         <Def>
-          <main>
-              <h1>PLACES</h1>
-          </main>
+            <main>
+                <h1>PLACES INDEX PAGE</h1>
+                {placesFormatted}
+            </main>
         </Def>
-    )
+    );
 };
 
 
