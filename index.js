318a76ecc26c5218466592b00174a066a3b09140
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-01-25 21:14:19 -0500

    updated index and new public folder

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+// Modules and Globals
+require('dotenv').config();
+const express = require('express');
+const app = express();
 
 
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
+// Express Settings
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+app.use(express.static('public'));
 
 
-app.use('/places', require('./controllers/places'))
-
+// Controllers & Routes
+app.use('/places', require('./controllers/places'));
 
 app.get('/', (req, res) => {
-  res.render('home')
-})
+    res.render('home')
+});
 
-// 404 error
 app.get('*', (req, res) => {
-  res.render('error404')
-})
+    res.render('error404')
+});
 
 
-app.listen(process.env.PORT)
+// Listen for Connections
+app.listen(process.env.PORT);
