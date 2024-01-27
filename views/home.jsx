04edbfc7876366d35e490980db68ff8bdbbba67d
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-01-27 17:24:05 -0500

    part 4 - step 4 completed

--- views/home.jsx
+++ views/home.jsx
@@ -8,6 +8,13 @@ function home () {
           <main>
               <h1>HOME</h1>
 
+              <div>
+                <img src="/images/pancakes-chad-montano-unsplash.jpg" alt="pancakes-chad-montano-unsplash" height="600vh" />
+                <div>
+                  Photo by <a href="https://unsplash.com/@briewilly?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Chad Montano</a> on <a href="https://unsplash.com/photos/baked-pancakes-eeqbbemH9-c?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+                </div>
+              </div>
+
               <a href="/places">
                 <button className="btn btn-primary">Places Page</button>
               </a>
