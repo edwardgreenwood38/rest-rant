08af5eaa566edf0fd5111d438f735d65f0ca588a
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-01-28 18:19:03 -0500

    part 5 - step 2 completed
    added controller for new view

--- controllers/places.js
+++ controllers/places.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 
 
 // GET /places
+router.get('/new', (req, res) => {
+  res.render('places/new');
+});
+
+
 router.get('/', (req, res) => {
     let places = [{
       name: 'H-Thai-ML',
