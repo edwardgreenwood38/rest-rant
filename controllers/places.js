44914206b2f39fe1c44f6da28186c1915f83389a
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-01-28 17:03:51 -0500

    part 4 - step 6 completed
    images for palces

--- controllers/places.js
+++ controllers/places.js
@@ -8,16 +8,22 @@ router.get('/', (req, res) => {
       city: 'Seattle',
       state: 'WA',
       cuisines: 'Thai, Pan-Asian',
-      pic: 'http://placekitten.com/250/250'
+      pic: '/images/sports-bar-tamara-malaniy-unsplash.jpg'
     }, {
       name: 'Coding Cat Cafe',
       city: 'Phoenix',
       state: 'AZ',
       cuisines: 'Coffee, Bakery',
-      pic: 'http://placekitten.com/250/250'
+      pic: '/images/cafe-mihai-moisa-unsplash.jpg'
     }]
     
     res.render('places/index', { places })
-  })
+  });
+
+// Photo by <a href="https://unsplash.com/@tamarushphotos?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Tamara Malaniy</a> on <a href="https://unsplash.com/photos/man-in-white-shirt-sitting-on-chair-4ec2BK0HRX8?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+
+// Photo by <a href="https://unsplash.com/@moisamihai092?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Mihai Moisa</a> on <a href="https://unsplash.com/photos/black-chairs-Djtc1T38-GY?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
+    
 
 module.exports = router;
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -5,9 +5,18 @@ const Def = require('../default');
 function places (data) {
     let placesFormatted = data.places.map((place) => {
         return (
-            <div>
+            <div className="col-sm-6">
                 <h2>{place.name}</h2>
-                <img src={place.pic} alt={place.name}></img>
+
+                <p className="text-center">
+                    {place.cuisines}
+                </p>
+                
+                <img src={place.pic} alt={place.name} height="350vh" />
+
+                <p className="text-center">
+                    Located in {place.city}, {place.state}
+                </p>
             </div>
         )
     });
@@ -15,8 +24,11 @@ function places (data) {
     return (
         <Def>
             <main>
-                <h1>PLACES INDEX PAGE</h1>
-                {placesFormatted}
+                <h1>Places to Rant or Rave About</h1>
+
+                <div className="row">
+                    {placesFormatted}
+                </div>
             </main>
         </Def>
     );
