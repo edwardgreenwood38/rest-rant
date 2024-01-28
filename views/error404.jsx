d1a7c28798f1054c986e492cd4971921ef08de36
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-01-28 16:48:59 -0500

    part 4 - step 5 completed
    404 page

--- views/error404.jsx
+++ views/error404.jsx
@@ -9,6 +9,13 @@ function error404 () {
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't find this page!</p>
+
+                <div>
+                <img src="/images/storm-trooper-daniel-k-cheung-unsplash.jpg" alt="storm-trooper-daniel-cheung-unsplash" height="400vh" />
+                <div>
+                    Photo by <a href="https://unsplash.com/@danielkcheung?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Daniel K Cheung</a> on <a href="https://unsplash.com/photos/stormtrooper-minifigure-walking-on-the-sand-cPF2nlWcMY4?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+                </div>
+              </div>
             </main>
         </Def>
     )
