1cbdfb64c3dedc25b8abadbdfd7c9c3ffec20a56
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-01-29 18:41:18 -0500

    part 5 - step 6 completed

---
+++ models/places.js
@@ -0,0 +1,20 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/sports-bar-tamara-malaniy-unsplash.jpg',
+    photoBy: 'Tamara Malaniy',
+    photoByLink: 'https://unsplash.com/@tamarushphotos?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+    onLink: 'https://unsplash.com/photos/man-in-white-shirt-sitting-on-chair-4ec2BK0HRX8?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/cafe-mihai-moisa-unsplash.jpg',
+    photoBy: 'Mihai Moisa',
+    photoByLink: 'https://unsplash.com/@moisamihai092?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+    onLink: 'https://unsplash.com/photos/black-chairs-Djtc1T38-GY?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash'
+  }]
+  
