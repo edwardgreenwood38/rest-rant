8ebe5106e341a08f9800be56bee3eba6eeaaaa61
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-01-30 21:30:44 -0500

    part 6 - step 2 show route updated

--- controllers/places.js
+++ controllers/places.js
@@ -30,6 +30,22 @@ router.get('/new', (req, res) => {
 });
 
 
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id);
+
+  if (isNaN(id)) {
+    res.render('error404');
+  }
+  else if (!places[id]) {
+    res.render('error404');
+  }
+  else {
+    res.render('places/show', { place: places[id] });
+  }
+  
+});
+
+
 router.get('/', (req, res) => {    
     res.render('places/index', { places })
   });
