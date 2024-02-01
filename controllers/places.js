2334e7241a9301eb5cd1a9d734a9c7a4e9924d83
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-01-31 20:33:01 -0500

    part 6 - step 6 delete
    not working.

--- controllers/places.js
+++ controllers/places.js
@@ -3,6 +3,22 @@ const places = require('../models/places.js');
 
 
 // Post /places
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id);
+
+  if (isNaN(id)) {
+    res.render('error404');
+  }
+  else if (!places[id]) {
+    res.render('error404');
+  }
+  else {
+    places.splice(id, 1);
+    res.redirect('/places');
+  }
+});
+
+
 router.post('/', (req, res) => {
   
   if (!req.body.pic) {
@@ -22,6 +38,7 @@ router.post('/', (req, res) => {
 
   res.redirect('/places');
 });
+
 
 
 // GET /places
